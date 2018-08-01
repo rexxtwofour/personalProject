3f154e69713e52702beb3f53b8048bcccc1732cb
Eric Rivera <ericrivera@Eric-Riveras-MacBook-Pro.local>
2018-07-31 17:10:04 -0700

    more organized bcrypt and sessions

--- wstransit_personal/server/auth_controller.js
+++ wstransit_personal/server/auth_controller.js
@@ -1,46 +1,65 @@
-const users = require('./users');
-let id = 1;
-
-module.exports = {
+//might not need this file, everything was put into index server file until this is checked off as good
 
 
-    app.post('/loggedin', (req, res) => {
-        const { username, password } = req.body;
-        app.get('db').find_user([username]).then(user => { 
-          if(user.length){
-            console.log(user)
-            const {id, name, email, username } = user[0];
-      // Use bcrypt.compare to compare the entered password with the hashed password.
-            bcrypt.compare(password, user[0].password).then(passwordMatch => {
-              if(passwordMatch){
-                req.session.user = { id, email, name, username }
-                res.status(200).send( req.session.user )
-              } else {
-                res.send({error: 'Wrong Username or Password'})
-              }
-            }).catch(err => console.log('Login 1', err))
-          } else {
-            res.send({error: 'No User Found'})
-          }
-        }).catch(() => res.status(403).send({error: 'Something went wrong. Please try again.'}))
-      })
-      
-      app.post('/register', (req, res) => {
-        const { name, email, username, password } = req.body;
-      //bcrypt.hash is what hashes the password. This needs to be done before inserting into the database.
-        bcrypt.hash( password, saltRounds ).then( hashedPassword => {
-      //create user sql file will be an insert statemetent with a RETURNING * at the end
-          app.get('db').create_user({name, email, username, hashedPassword}).then(user => {
-            req.session.user = user;
-            res.status(200).send(req.session.user)
-          }).catch(err => {
-            console.log('Login 2',err);
-            res.status(200).send({error: 'Something went wrong, please try a different username or try again.'})
-          })
-        })
-      })
 
 
+// const express = require('express');
+// const bodyPaser = require('body-parser');
+// const session = require('express-session');
+// const massive = require('massive');
+// const bcrypt = require('bcrypt');
+// const numOfSaltRounds = 12;
+// const users = require('./users');
+// let id = 1;
+
+// module.exports = {
+
+// login: 
+//     app.post('/login', (req, res) => {
+//         const { username, password } = req.body;
+//         app.get('db').find_user([username]).then(user => { 
+//           if(user.length){
+//             console.log(user)
+//             const {id, name, email } = user[0];
+//       // Use bcrypt.compare to compare the entered password with the hashed password.
+//             bcrypt.compare(password, user[0].password).then(passwordMatch => {
+//               if(passwordMatch){
+//                 req.session.user = { id, name ,email, }
+//                 res.status(200).send( req.session.user )
+//               } else {
+//                 res.send({error: 'Wrong Username or Password'})
+//               }
+//             }).catch(err => console.log('Login 1', err))
+//           } else {
+//             res.send({error: 'No User Found'})
+//           }
+//         }).catch(() => res.status(403).send({error: 'Something went wrong. Please try again.'}))
+//       }),
+
+
+
+// register:
+//       app.post('/register', (req, res) => {
+//         const { name, email, password } = req.body;
+//       //bcrypt.hash is what hashes the password. This needs to be done before inserting into the database.
+//         bcrypt.hash( password, saltRounds ).then( hashedPassword => {
+//       //create user sql file will be an insert statemetent with a RETURNING * at the end
+//           app.get('db').create_user({name, email, hashedPassword}).then(user => {
+//             req.session.user = user;
+//             res.status(200).send(req.session.user)
+//           }).catch(err => {
+//             console.log('Login 2',err);
+//             res.status(200).send({error: 'Something went wrong, please try a different username or try again.'})
+//           })
+//         })
+//       }),
+
+// logout: 
+
+// app.post('/logout', (req, res) => {
+//     req.session.destroy();
+//     res.status(200).send();
+//   })
 
 
 
--- wstransit_personal/server/index.js
+++ wstransit_personal/server/index.js
@@ -4,6 +4,13 @@ const massive = require('massive');
 const ctrl= require('./controller');
 require('dotenv').config();
 
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const numOfSaltRounds = 12;
+const users = require('./users');
+let id = 1;
+
+
 
 
 
@@ -27,10 +34,6 @@ app.post( '/api/login', auth_controller.login );
 
 
 
-
-
-
-
 const app = express();
 app.use(bodyParser.json());
 
@@ -51,6 +54,85 @@ const path = require('path')
 app.get('*', (req, res)=>{
   res.sendFile(path.join(__dirname, '../build/index.html'));
 })
+
+
+
+
+
+
+
+
+
+//BCrypt
+
+
+module.exports = {
+
+    login: 
+        app.post('/login', (req, res) => {
+            const { username, password } = req.body;
+            app.get('db').find_users([username]).then(users => { 
+              if(user.length){
+                console.log(users)
+                const {id, name, email } = users[0];
+          // Use bcrypt.compare to compare the entered password with the hashed password.
+                bcrypt.compare(password, users[0].password).then(passwordMatch => {
+                  if(passwordMatch){
+                    req.session.users = { id, name ,email, }
+                    res.status(200).send( req.session.users )
+                  } else {
+                    res.send({error: 'Wrong Username or Password'})
+                  }
+                }).catch(err => console.log('Login 1', err))
+              } else {
+                res.send({error: 'No User Found'})
+              }
+            }).catch(() => res.status(403).send({error: 'Something went wrong. Please try again.'}))
+          }),
+    
+    
+    
+    register:
+          app.post('/register', (req, res) => {
+            const { name, email, password } = req.body;
+          //bcrypt.hash is what hashes the password. This needs to be done before inserting into the database.
+            bcrypt.hash( password, saltRounds ).then( hashedPassword => {
+          //create user sql file will be an insert statemetent with a RETURNING * at the end
+              app.get('db').create_user({name, email, hashedPassword}).then(users => {
+                req.session.user = users;
+                res.status(200).send(req.session.users)
+              }).catch(err => {
+                console.log('Login 2',err);
+                res.status(200).send({error: 'Something went wrong, please try a different username or try again.'})
+              })
+            })
+          }),
+    
+    logout: 
+    
+    app.post('/logout', (req, res) => {
+        req.session.destroy();
+        res.status(200).send();
+      })
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
